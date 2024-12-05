a5d7fdce709fd9f16b517de50efe30e24f7d546f
KIET <anhkiet07012003@gmail.com>
2024-12-05 21:24:54 +0700

    delete all

--- sprint5/UI/src/app/models/pagination.ts
+++
@@ -1,9 +0,0 @@
-export class Pagination<PaginationObject> {
-  constructor(
-    public readonly data: PaginationObject[],
-    public readonly per_page: number,
-    public readonly total: number,
-    public readonly last_page: number
-  ) {
-  }
-}
